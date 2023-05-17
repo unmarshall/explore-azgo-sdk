bf915b75affc8db45e16fa5eb661d45aba30512a
Madhav Bhargava <madhav.bhargava@sap.com>
2023-05-17 17:20:47 +0530

    added more stuff to createvm.go

--- vm/create/createvm.go
+++ vm/create/createvm.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"errors"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v4"
 	"github.com/unmarshall/explore-azgo-sdk/common"
 	"k8s.io/utils/pointer"
@@ -49,6 +52,7 @@ func main() {
 	common.DieOnError(err, "polling for vm created failed")
 
 	createdVM := resp.VirtualMachine
+	fmt.Printf("created VM: %v\n", createdVM)
 
 }
 
@@ -60,44 +64,53 @@ func createVMParameters() armcompute.VirtualMachine {
 	}
 
 	vmSize := armcompute.VirtualMachineSizeTypesStandardA4V2
-	encodedUserData := base64.StdEncoding.EncodeToString([]byte(secret.Data["userData"]))
+	helloCustomScript := "IyEvdXNyL2Jpbi9lbnYgYmFzaAoKZWNobyAiaGVsbG8i"
 
 	vmProperties := armcompute.VirtualMachineProperties{
-		AdditionalCapabilities: nil,
-		ApplicationProfile:     nil,
-		AvailabilitySet:        nil,
-		BillingProfile:         nil,
-		CapacityReservation:    nil,
-		DiagnosticsProfile:     nil,
-		EvictionPolicy:         nil,
-		ExtensionsTimeBudget:   nil,
 		HardwareProfile: &armcompute.HardwareProfile{
 			VMSize: &vmSize,
 		},
-		NetworkProfile: nil,
+		NetworkProfile: &armcompute.NetworkProfile{
+			NetworkAPIVersion:              nil,
+			NetworkInterfaceConfigurations: nil,
+			NetworkInterfaces:              nil,
+		},
 		OSProfile: &armcompute.OSProfile{
-			AdminPassword:               nil,
-			AdminUsername:               pointer.String(adminUserName),
-			AllowExtensionOperations:    nil,
-			ComputerName:                pointer.String(vmName),
-			CustomData:                  nil,
-			LinuxConfiguration:          nil,
+			AdminUsername: pointer.String(adminUserName),
+			ComputerName:  pointer.String(vmName),
+			CustomData:    &helloCustomScript,
+			LinuxConfiguration: &armcompute.LinuxConfiguration{
+				DisablePasswordAuthentication: pointer.Bool(true),
+				SSH: &armcompute.SSHConfiguration{
+					PublicKeys: []*armcompute.SSHPublicKey{
+						createSSHPublicKey(),
+					},
+				},
+			},
 			RequireGuestProvisionSignal: nil,
 			Secrets:                     nil,
 			WindowsConfiguration:        nil,
 		},
-		PlatformFaultDomain:     nil,
-		Priority:                nil,
-		ProximityPlacementGroup: nil,
-		ScheduledEventsProfile:  nil,
-		SecurityProfile:         nil,
-		StorageProfile:          nil,
-		UserData:                nil,
-		VirtualMachineScaleSet:  nil,
-		InstanceView:            nil,
-		ProvisioningState:       nil,
-		TimeCreated:             nil,
-		VMID:                    nil,
+		StorageProfile: &armcompute.StorageProfile{
+			DataDisks:      createDataDisks(),
+			ImageReference: createImageReference(),
+			OSDisk: &armcompute.OSDisk{
+				CreateOption: to.Ptr(armcompute.DiskCreateOptionTypesFromImage),
+				Caching:      to.Ptr(armcompute.CachingTypesNone),
+				DeleteOption: to.Ptr(armcompute.DiskDeleteOptionTypesDelete),
+				DiskSizeGB:   pointer.Int32(50),
+				ManagedDisk: &armcompute.ManagedDiskParameters{
+					StorageAccountType: to.Ptr(armcompute.StorageAccountTypesStandardSSDLRS),
+				},
+				Name: to.Ptr(vmName + "-os-disk"),
+			},
+		},
+		UserData:               nil,
+		VirtualMachineScaleSet: nil,
+		InstanceView:           nil,
+		ProvisioningState:      nil,
+		TimeCreated:            nil,
+		VMID:                   nil,
 	}
 
 	return armcompute.VirtualMachine{
@@ -105,13 +118,61 @@ func createVMParameters() armcompute.VirtualMachine {
 		ExtendedLocation: nil,
 		Identity:         nil,
 		Plan:             &plan,
-		Properties:       nil,
+		Properties:       &vmProperties,
 		Tags:             nil,
 		Zones:            nil,
 		ID:               nil,
 		Name:             pointer.String(vmName),
 		Resources:        nil,
 		Type:             nil,
+	}
+}
+
+func createSSHPublicKey() *armcompute.SSHPublicKey {
+	homeDir, err := os.UserHomeDir()
+	authorizedKeysPath := filepath.Join(homeDir, ".ssh/authorized_keys")
+	common.DieOnError(err, "failed to get home directory")
+	sshPublicKeyPath := filepath.Join(homeDir, ".ssh/id_rsa.pub")
+	var sshBytes []byte
+	if _, err := os.Stat(sshPublicKeyPath); err != nil {
+		common.DieOnError(err, "failed to open path: "+sshPublicKeyPath)
+	}
+	sshBytes, err = os.ReadFile(sshPublicKeyPath)
+	common.DieOnError(err, "failed to read file at: "+sshPublicKeyPath)
+	return &armcompute.SSHPublicKey{
+		KeyData: to.Ptr(string(sshBytes)),
+		Path:    to.Ptr(fmt.Sprintf(authorizedKeysPath)),
+	}
+}
+
+func createDataDisks() []*armcompute.DataDisk {
+	return []*armcompute.DataDisk{
+		{
+			CreateOption: to.Ptr(armcompute.DiskCreateOptionTypesEmpty),
+			Lun:          pointer.Int32(1),
+			Caching:      to.Ptr(armcompute.CachingTypesReadWrite),
+			DeleteOption: to.Ptr(armcompute.DiskDeleteOptionTypesDelete),
+			DiskSizeGB:   pointer.Int32(10),
+			ManagedDisk: &armcompute.ManagedDiskParameters{
+				StorageAccountType: to.Ptr(armcompute.StorageAccountTypesStandardSSDLRS),
+			},
+			Name: to.Ptr("mb-1"),
+		},
+	}
+}
+
+func createImageReference() *armcompute.ImageReference {
+	urn := "sap:gardenlinux:greatest:934.7.0"
+	splits := strings.Split(urn, ":")
+	publisher := splits[0]
+	offer := splits[1]
+	sku := splits[2]
+	version := splits[3]
+	return &armcompute.ImageReference{
+		Publisher: &publisher,
+		Offer:     &offer,
+		SKU:       &sku,
+		Version:   &version,
 	}
 }
 
